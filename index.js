630c4765f097c77e431115277fccd738353a78f1
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-02 17:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ const createFareMultiplier = function(value) {
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, func) {
+  return func(drivers);
+}
